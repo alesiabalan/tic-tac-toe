163047ad0ff38abbb86d2e2485f3aa6ee547cce0
Alesia Balan <alesia.laktionova@gmail.com>
2019-10-20 13:09:06 +0300

    Implement tic-tac-toe

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,91 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.game = [[], [], []];
+    this.x = null;
+    this.o = null;
+  }
 
+  getCurrentPlayerSymbol() {
+    return !this.game ? "x" : this.x > this.o ? "o" : "x";
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    let symbol = this.getCurrentPlayerSymbol();
+    if (!this.game[rowIndex][columnIndex]) {
+      this.game[rowIndex][columnIndex] = symbol;
+      this[symbol]++;
     }
+  }
 
-    getCurrentPlayerSymbol() {
+  isFinished() {
+    return this.getWinner() || this.noMoreTurns() ? true : false;
+  }
 
+  getWinner() {
+    if (this.x > this.o) {
+      for (let i = 0; i <= 2; i++) {
+        if (
+          (this.game[i][0] &&
+            this.game[i][0] === this.game[i][1] &&
+            this.game[i][0] === this.game[i][2]) ||
+          (this.game[0][i] &&
+            this.game[0][i] === this.game[1][i] &&
+            this.game[0][i] === this.game[2][i]) ||
+          (this.game[0][0] &&
+            this.game[0][0] === this.game[1][1] &&
+            this.game[0][0] === this.game[2][2]) ||
+          (this.game[0][2] &&
+            this.game[0][2] === this.game[1][1] &&
+            this.game[0][2] === this.game[2][0])
+        ) {
+          return "x";
+        }
+      }
+      return null;
+    } else {
+      if (this.x === this.o) {
+        for (let i = 0; i <= 2; i++) {
+          if (
+            (this.game[i][0] &&
+              this.game[i][0] === this.game[i][1] &&
+              this.game[i][0] === this.game[i][2]) ||
+            (this.game[0][i] &&
+              this.game[0][i] === this.game[1][i] &&
+              this.game[0][i] === this.game[2][i]) ||
+            (this.game[0][0] &&
+              this.game[0][0] === this.game[1][1] &&
+              this.game[0][0] === this.game[2][2]) ||
+            (this.game[0][2] &&
+              this.game[0][2] === this.game[1][1] &&
+              this.game[0][2] === this.game[2][0])
+          ) {
+            return "o";
+          }
+        }
+        return null;
+      }
     }
+  }
 
-    nextTurn(rowIndex, columnIndex) {
+  noMoreTurns() {
+    return this.x + this.o === 9;
+  }
 
+  isDraw() {
+    if (!this.getWinner() && this.noMoreTurns()) {
+      return true;
     }
+    return false;
+  }
 
-    isFinished() {
-
+  getFieldValue(rowIndex, colIndex) {
+    if (rowIndex <= 2 && colIndex <= 2) {
+      return !this.game[rowIndex][colIndex]
+        ? null
+        : this.game[rowIndex][colIndex];
     }
-
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+    return null;
+  }
 }
 
 module.exports = TicTacToe;
